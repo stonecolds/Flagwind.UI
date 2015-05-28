29f785cd7786cac5ea58e0d77df976b9efe9fac0
jason <jasonsoop@gmail.com>
2015-05-28 18:34:27 +0800

    初始化脚本文件。

---
+++ js/flagwind.js
@@ -0,0 +1,8 @@
+/*!
+ * Flagwind.UI v1.0.0
+ * Copyright 2014 Flagwind Inc. All rights reserved.
+ * Licensed under the MIT License.
+ * https://github.com/Flagwind/Flagwind.UI/blob/master/LICENSE
+!*/
+
+// @codekit-append "ui-dropdown.js";
